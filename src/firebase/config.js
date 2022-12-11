dff66f3620dc6f0b36794fe4ea0faa82cf915c64
MinnAK686 <anastasioak327@gmail.com>
2022-12-11 16:21:53 +0630

    Project Setup

---
+++ src/firebase/config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD4QAtcr0SWsAKF_56HKzi5g2YWskXgevw",
+    authDomain: "back-end-service.firebaseapp.com",
+    projectId: "back-end-service",
+    storageBucket: "back-end-service.appspot.com",
+    messagingSenderId: "392417906562",
+    appId: "1:392417906562:web:836dddf6ab1afbed14c99b",
+};
--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
 
-createApp(App).use(router).mount('#app')
+// MDB
+import "mdb-ui-kit/css/mdb.min.css";
+import "mdb-ui-kit/js/mdb.min.js";
+
+createApp(App).use(router).mount("#app");
